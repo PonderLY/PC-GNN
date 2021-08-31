b3b1534fc1dbe0c25cf7b91329a48fdcfe355369
PonderLY <liuyang173@mails.ucas.edu.cn>
2021-08-31 16:14:10 +0800

    fix bugs and add comments

--- src/layers.py
+++ src/layers.py
@@ -94,13 +94,11 @@ class InterAgg(nn.Module):
 		# calculate label-aware scores
 		if self.cuda:
 			batch_features = self.features(torch.cuda.LongTensor(list(unique_nodes)))
-		else:
-			batch_features = self.features(torch.LongTensor(list(unique_nodes)))
-		batch_scores = self.label_clf(batch_features)
-		if self.cuda:
 			pos_features = self.features(torch.cuda.LongTensor(list(self.train_pos)))
 		else:
+			batch_features = self.features(torch.LongTensor(list(unique_nodes)))
 			pos_features = self.features(torch.LongTensor(list(self.train_pos)))
+		batch_scores = self.label_clf(batch_features)
 		pos_scores = self.label_clf(pos_features)
 		id_mapping = {node_id: index for node_id, index in zip(unique_nodes, range(len(unique_nodes)))}
 
@@ -124,11 +122,12 @@ class InterAgg(nn.Module):
 
 		# intra-aggregation steps for each relation
 		# Eq. (8) in the paper
-		r1_feats, r1_scores = self.intra_agg1.forward(nodes, labels, r1_list, center_scores, r1_scores, pos_scores, r1_sample_num_list)
-		r2_feats, r2_scores = self.intra_agg2.forward(nodes, labels, r2_list, center_scores, r2_scores, pos_scores, r2_sample_num_list)
-		r3_feats, r3_scores = self.intra_agg3.forward(nodes, labels, r3_list, center_scores, r3_scores, pos_scores, r3_sample_num_list)
+		r1_feats, r1_scores = self.intra_agg1.forward(nodes, labels, r1_list, center_scores, r1_scores, pos_scores, r1_sample_num_list, train_flag)
+		r2_feats, r2_scores = self.intra_agg2.forward(nodes, labels, r2_list, center_scores, r2_scores, pos_scores, r2_sample_num_list, train_flag)
+		r3_feats, r3_scores = self.intra_agg3.forward(nodes, labels, r3_list, center_scores, r3_scores, pos_scores, r3_sample_num_list, train_flag)
 
 		# concat the intra-aggregated embeddings from each relation
+		# Eq. (9) in the paper
 		neigh_feats = torch.cat((r1_feats, r2_feats, r3_feats), dim=0)
 
 		# get features or embeddings for batch nodes
@@ -142,7 +141,7 @@ class InterAgg(nn.Module):
 		n = len(nodes)
 
 		# inter-relation aggregation steps
-		
+		# Eq. (9) in the paper
 		if self.inter == 'Att':
 			# 1) Att Inter-relation Aggregator
 			combined, attention = att_inter_agg(len(self.adj_lists), self.leakyrelu, self_feats, neigh_feats, self.embed_dim,
@@ -180,21 +179,26 @@ class IntraAgg(nn.Module):
 		self.train_pos = train_pos
 		self.rho = rho
 
-	def forward(self, nodes, batch_labels, to_neighs_list, batch_scores, neigh_scores, pos_scores, sample_list):
+	def forward(self, nodes, batch_labels, to_neighs_list, batch_scores, neigh_scores, pos_scores, sample_list, train_flag):
 		"""
 		Code partially from https://github.com/williamleif/graphsage-simple/
 		:param nodes: list of nodes in a batch
 		:param to_neighs_list: neighbor node id list for each batch node in one relation
 		:param batch_scores: the label-aware scores of batch nodes
 		:param neigh_scores: the label-aware scores 1-hop neighbors each batch node in one relation
+		:param pos_scores: the label-aware scores 1-hop neighbors for the minority positive nodes
+		:param train_flag: indicates whether in training or testing mode
 		:param sample_list: the number of neighbors kept for each batch node in one relation
 		:return to_feats: the aggregated embeddings of batch nodes neighbors in one relation
 		:return samp_scores: the average neighbor distances for each relation after filtering
 		"""
 
-		# filer neighbors under given relation
-		samp_neighs, samp_scores = choose_step_neighs(batch_scores, batch_labels, neigh_scores, to_neighs_list, pos_scores, self.train_pos, sample_list, self.rho)
-
+		# filer neighbors under given relation in the train mode
+		if train_flag:
+			samp_neighs, samp_scores = choose_step_neighs(batch_scores, batch_labels, neigh_scores, to_neighs_list, pos_scores, self.train_pos, sample_list, self.rho)
+		else:
+			samp_neighs, samp_scores = choose_step_test(batch_scores, neigh_scores, to_neighs_list, sample_list)
+		
 		# find the unique nodes among batch nodes and the filtered neighbors
 		unique_nodes_list = list(set.union(*samp_neighs))
 		unique_nodes = {n: i for i, n in enumerate(unique_nodes_list)}
@@ -219,10 +223,13 @@ class IntraAgg(nn.Module):
 
 def choose_step_neighs(center_scores, center_labels, neigh_scores, neighs_list, minor_scores, minor_list, sample_list, sample_rate):
     """
-    Filter neighbors according label predictor result with adaptive thresholds
+    Choose step for neighborhood sampling
     :param center_scores: the label-aware scores of batch nodes
+    :param center_labels: the label of batch nodes
     :param neigh_scores: the label-aware scores 1-hop neighbors each batch node in one relation
     :param neighs_list: neighbor node id list for each batch node in one relation
+	:param minor_scores: the label-aware scores for nodes of minority class in one relation
+    :param minor_list: minority node id list for each batch node in one relation
     :param sample_list: the number of neighbors kept for each batch node in one relation
 	:para sample_rate: the ratio of the oversample neighbors for the minority class
     :return samp_neighs: the neighbor indices and neighbor simi scores
@@ -238,13 +245,11 @@ def choose_step_neighs(center_scores, center_labels, neigh_scores, neighs_list, 
         num_sample = sample_list[idx]
 
         # compute the L1-distance of batch nodes and their neighbors
-        
         score_diff_neigh = torch.abs(center_score_neigh - neigh_score).squeeze()
         sorted_score_diff_neigh, sorted_neigh_indices = torch.sort(score_diff_neigh, dim=0, descending=False)
         selected_neigh_indices = sorted_neigh_indices.tolist()
 
-        # top-p sampling according to distance ranking and thresholds
-        
+        # top-p sampling according to distance ranking
         if len(neigh_scores[idx]) > num_sample + 1:
             selected_neighs = [neighs_indices[n] for n in selected_neigh_indices[:num_sample]]
             selected_score_diff = sorted_score_diff_neigh.tolist()[:num_sample]
@@ -267,6 +272,47 @@ def choose_step_neighs(center_scores, center_labels, neigh_scores, neighs_list, 
         samp_score_diff.append(selected_score_diff)
 
     return samp_neighs, samp_score_diff
+
+
+def choose_step_test(center_scores, neigh_scores, neighs_list, sample_list):
+	"""
+	Filter neighbors according label predictor result with adaptive thresholds
+	:param center_scores: the label-aware scores of batch nodes
+	:param neigh_scores: the label-aware scores 1-hop neighbors each batch node in one relation
+	:param neighs_list: neighbor node id list for each batch node in one relation
+	:param sample_list: the number of neighbors kept for each batch node in one relation
+	:return samp_neighs: the neighbor indices and neighbor simi scores
+	:return samp_scores: the average neighbor distances for each relation after filtering
+	"""
+
+	samp_neighs = []
+	samp_scores = []
+	for idx, center_score in enumerate(center_scores):
+		center_score = center_scores[idx][0]
+		neigh_score = neigh_scores[idx][:, 0].view(-1, 1)
+		center_score = center_score.repeat(neigh_score.size()[0], 1)
+		neighs_indices = neighs_list[idx]
+		num_sample = sample_list[idx]
+
+		# compute the L1-distance of batch nodes and their neighbors
+		score_diff = torch.abs(center_score - neigh_score).squeeze()
+		sorted_scores, sorted_indices = torch.sort(score_diff, dim=0, descending=False)
+		selected_indices = sorted_indices.tolist()
+
+		# top-p sampling according to distance ranking and thresholds
+		if len(neigh_scores[idx]) > num_sample + 1:
+			selected_neighs = [neighs_indices[n] for n in selected_indices[:num_sample]]
+			selected_scores = sorted_scores.tolist()[:num_sample]
+		else:
+			selected_neighs = neighs_indices
+			selected_scores = score_diff.tolist()
+			if isinstance(selected_scores, float):
+				selected_scores = [selected_scores]
+
+		samp_neighs.append(set(selected_neighs))
+		samp_scores.append(selected_scores)
+
+	return samp_neighs, samp_scores
 
 
 def mean_inter_agg(num_relations, self_feats, neigh_feats, embed_dim, weight, n, cuda):
@@ -395,7 +441,6 @@ def att_inter_agg(num_relations, att_layer, self_feats, neigh_feats, embed_dim, 
 def threshold_inter_agg(num_relations, self_feats, neigh_feats, embed_dim, weight, threshold, n, cuda):
 	"""
 	GNN inter-relation aggregator
-	
 	:param num_relations: number of relations in the graph
 	:param self_feats: batch nodes features or embeddings
 	:param neigh_feats: intra-relation aggregated neighbor embeddings for each relation
